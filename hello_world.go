b6510da9620ff535d3941a80b0299c7e73c21975
zrthstr <zrth1k@gmail.com>
2016-07-13 23:13:28 +0200

    blub

--- hello_world.go
+++ hello_world.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	//"fmt"
+    "fmt"
     "html/template"
-	"net/http"
+	"log"
+    "net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +13,28 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, "hi")
 }
 
+
+func getBook(book_url string){
+    fmt.Printf("trying to get book %s", book_url)
+}
+
+func post_handler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    book_url := r.FormValue("book_url")
+    fmt.Fprintf(w, "path:%s<br>", r.URL.Path[1:])
+    fmt.Fprintf(w, "book_url:%s<br>", book_url)
+    // validate res
+    getBook(book_url)
+}
+
+
+
 func main() {
     http.HandleFunc("/", indexHandler)
-
     static := http.FileServer( http.Dir( "static" ) )
     http.Handle("/static/", http.StripPrefix("/static/", static))
-
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/book", post_handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+
