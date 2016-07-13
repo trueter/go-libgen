88b1f10c6a8c7606483d5a26901a0e09383dfc32
Torsten Rü <torsten@rueter.net>
2016-07-13 23:51:00 +0200

    convert indentation to spaces

--- hello_world.go
+++ hello_world.go
@@ -3,14 +3,14 @@ package main
 import (
     "fmt"
     "html/template"
-	"log"
+    "log"
     "net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("form.html")
-	t.Execute(w, "hi")
+    t, _ := template.ParseFiles("form.html")
+    t.Execute(w, "hi")
 }
 
 
@@ -34,7 +34,7 @@ func main() {
     static := http.FileServer( http.Dir( "static" ) )
     http.Handle("/static/", http.StripPrefix("/static/", static))
     http.HandleFunc("/book", post_handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
