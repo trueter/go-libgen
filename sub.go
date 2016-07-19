f43966ff0b0e017f6bcacfaa989778a40569614c
zrthstr <zrth1k@gmail.com>
2016-07-19 22:01:36 +0200

    created subscriber sub.go

---
+++ sub.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	    "fmt"
+		//"log"
+		"gopkg.in/redis.v4"
+)
+
+func main(){
+
+    client := redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+    Password: "", // no password set
+	DB:       0,  // use default DB
+	})
+
+	err := client.Publish("tasks", "hello").Err()
+		if err != nil {
+	    panic(err)
+	}
+	for {
+	pubsub, err:= client.Subscribe("tasks")
+		if err != nil {
+	panic(err)
+	}
+
+	msg, err:= pubsub.ReceiveMessage()
+	if err != nil {
+	    panic(err)
+	}
+
+	fmt.Println(msg.Channel, msg.Payload)
+	}
+
+}
